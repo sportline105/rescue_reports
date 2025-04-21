eb7c698916f2e5ec8ff5f4549d1816b5530dc2cb
sportline105 <norbert616@freenet.de>
2025-04-21 05:20:47 +0200

    sicherung

---
+++ tx_firefighter_domain_model_event.php
@@ -0,0 +1,189 @@
+<?php
+return [
+    'ctrl' => [
+        'title' => 'LLL:EXT:firefighter/Resources/Private/Language/locallang_db.xlf:tx_firefighter_domain_model_event',
+        'label' => 'title',
+        'tstamp' => 'tstamp',
+        'crdate' => 'crdate',
+        'cruser_id' => 'cruser_id',
+        'versioningWS' => true,
+        'languageField' => 'sys_language_uid',
+        'transOrigPointerField' => 'l18n_parent',
+        'transOrigDiffSourceField' => 'l18n_diffsource',
+        'delete' => 'deleted',
+        'ignorePageTypeRestriction' => true,
+        'enablecolumns' => [
+            'disabled' => 'hidden',
+            'starttime' => 'starttime',
+            'endtime' => 'endtime',
+        ],
+        'searchFields' => 'title,description',
+        'iconfile' => 'EXT:firefighter/Resources/Public/Icons/tx_firefighter_domain_model_event.png'
+    ],
+'types' => [
+    '1' => [
+        'showitem' => 'title, description, date, cars, types, images, stations, brigade, deployments, --div--;Access, hidden, starttime, endtime'
+    ],
+],
+
+
+    'columns' => [
+        'sys_language_uid' => [
+            'exclude' => true,
+            'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.language',
+            'config' => [
+                'type' => 'language'
+            ]
+        ],
+        'l18n_parent' => [
+            'displayCond' => 'FIELD:sys_language_uid:>:0',
+            'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.l18n_parent',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'items' => [
+                    ['', 0],
+                ],
+                'foreign_table' => 'tx_firefighter_domain_model_event',
+                'foreign_table_where' => 'AND {#tx_firefighter_domain_model_event}.{#pid}=###CURRENT_PID### AND {#tx_firefighter_domain_model_event}.{#sys_language_uid} IN (-1,0)',
+                'default' => 0,
+            ]
+        ],
+        'l18n_diffsource' => [
+            'config' => [
+                'type' => 'passthrough'
+            ]
+        ],
+        'hidden' => [
+            'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.hidden',
+            'config' => [
+                'type' => 'check',
+                'items' => [
+                    [
+                'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.disable',
+                1
+            ]
+                ],
+            ]
+        ],
+        'starttime' => [
+            'exclude' => true,
+            'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.starttime',
+            'config' => [
+                'type' => 'input',
+                'renderType' => 'inputDateTime',
+                'eval' => 'datetime',
+                'default' => 0,
+            ]
+        ],
+        'endtime' => [
+            'exclude' => true,
+            'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.endtime',
+            'config' => [
+                'type' => 'input',
+                'renderType' => 'inputDateTime',
+                'eval' => 'datetime',
+                'default' => 0,
+                'range' => [
+                    'upper' => mktime(0, 0, 0, 1, 1, 2038),
+                ]
+            ]
+        ],
+        'title' => [
+            'label' => 'Title',
+            'config' => [
+                'type' => 'input',
+                'eval' => 'trim,required'
+            ]
+        ],
+        'description' => [
+            'label' => 'Description',
+            'config' => [
+                'type' => 'text',
+                'enableRichtext' => true,
+                'richtextConfiguration' => 'default', //oder 'minimal', 'full'
+                'rows' => 5
+            ]
+        ],
+       'date' => [
+    'label' => 'LLL:EXT:firefighter/Resources/Private/Language/locallang_db.xlf:tx_firefighter_domain_model_event.date',
+    'config' => [
+        'type' => 'input',
+        'renderType' => 'inputDateTime',
+        'eval' => 'datetime,null',
+        'dbType' => 'datetime',
+        'default' => null,
+    ],
+],
+'cars' => [
+    'label' => 'Fahrzeuge',
+    'config' => [
+        'type' => 'select',
+        'renderType' => 'selectMultipleSideBySide',
+        'foreign_table' => 'tx_firefighter_domain_model_car',
+        'MM' => 'tx_firefighter_event_car_mm',
+        'size' => 5,
+        'maxitems' => 9999,
+    ]
+],
+'types' => [
+    'label' => 'Einsatzarten',
+    'config' => [
+        'type' => 'select',
+        'renderType' => 'selectSingle',
+        'foreign_table' => 'tx_firefighter_domain_model_type',
+        'MM' => 'tx_firefighter_event_type_mm',
+        'size' => 5,
+        'maxitems' => 9999,
+    ]
+],
+'images' => [
+    'label' => 'Bild',
+    'config' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::getFileFieldTCAConfig(
+        'image',
+        [
+            'maxitems' => 10,
+            'appearance' => [
+                'createNewRelationLinkTitle' => 'LLL:EXT:core/Resources/Private/Language/locallang_core.xlf:label.addFileReference'
+            ],
+        ],
+        $GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext'] // z.B. 'jpg,jpeg,png,gif'
+    ),
+],
+
+'stations' => [
+    'label' => 'Stationen',
+    'config' => [
+        'type' => 'select',
+        'renderType' => 'selectMultipleSideBySide',
+        'foreign_table' => 'tx_firefighter_domain_model_station',
+        'MM' => 'tx_firefighter_event_station_mm',
+        'size' => 5,
+        'maxitems' => 9999,
+    ]
+],
+'brigade' => [
+    'label' => 'Feuerwehr',
+    'config' => [
+        'type' => 'select',
+        'renderType' => 'selectSingle',
+        'foreign_table' => 'tx_firefighter_domain_model_brigade',
+        'minitems' => 0,
+        'maxitems' => 1,
+    ]
+],
+'deployments' => [
+    'label' => 'Einsätze',
+    'config' => [
+        'type' => 'select',
+        'renderType' => 'selectMultipleSideBySide',
+        'foreign_table' => 'tx_firefighter_domain_model_deployment',
+        'MM' => 'tx_firefighter_event_deployment_mm', // MM-Tabelle angeben
+        'size' => 10,
+        'minitems' => 0,
+        'maxitems' => 9999,
+    ]
+],
+
+    ]
+];
