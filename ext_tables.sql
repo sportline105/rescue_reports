c03769ecb9839daac78204b5ba67faf0ef968760
sportline105 <norbert616@freenet.de>
2025-04-22 02:26:10 +0200

    Update ext_tables.sql

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,9 +1,12 @@
-CREATE TABLE tx_rescue_reports_domain_model_event (
+-- Domain-Model-Tabellen
+CREATE TABLE tx_firefighter_domain_model_event (
   uid int(11) NOT NULL auto_increment,
   pid int(11) DEFAULT '0' NOT NULL,
   title varchar(255) DEFAULT '' NOT NULL,
   description text,
-  date datetime DEFAULT NULL,
+  start datetime DEFAULT NULL,
+  end datetime DEFAULT NULL,
+  location varchar(255) DEFAULT '' NOT NULL,
   hidden tinyint(4) DEFAULT '0' NOT NULL,
   deleted tinyint(4) DEFAULT '0' NOT NULL,
   tstamp int(11) DEFAULT '0' NOT NULL,
@@ -23,10 +26,44 @@ CREATE TABLE tx_rescue_reports_domain_model_event (
   t3ver_count int(11) DEFAULT '0' NOT NULL,
   t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
   t3_origuid int(11) DEFAULT '0' NOT NULL,
+  cars int(11) DEFAULT '0' NOT NULL,
+  types int(11) DEFAULT '0' NOT NULL,
+  images INT(11) DEFAULT 0 NOT NULL,
+  brigade INT(11) DEFAULT 0 NOT NULL,
+  stations int(11) DEFAULT '0' NOT NULL,
+  deployments int(11) DEFAULT '0' NOT NULL,
   PRIMARY KEY (uid)
 );
 
-CREATE TABLE tx_rescue_reports_domain_model_car (
+CREATE TABLE tx_firefighter_domain_model_car (
+  uid int(11) NOT NULL auto_increment,
+  pid int(11) DEFAULT '0' NOT NULL,
+  name varchar(255) DEFAULT '' NOT NULL,
+  link varchar(255) DEFAULT '' NOT NULL,
+  hidden tinyint(4) DEFAULT '0' NOT NULL,
+  deleted tinyint(4) DEFAULT '0' NOT NULL,
+  tstamp int(11) DEFAULT '0' NOT NULL,
+  crdate int(11) DEFAULT '0' NOT NULL,
+  cruser_id int(11) DEFAULT '0' NOT NULL,
+  sys_language_uid int(11) DEFAULT '0' NOT NULL,
+  l18n_parent int(11) DEFAULT '0' NOT NULL,
+  l18n_diffsource mediumblob,
+  starttime int(11) DEFAULT '0' NOT NULL,
+  endtime int(11) DEFAULT '0' NOT NULL,
+  image INT(11) DEFAULT 0 NOT NULL,
+  t3ver_oid int(11) DEFAULT '0' NOT NULL,
+  t3ver_id int(11) DEFAULT '0' NOT NULL,
+  t3ver_wsid int(11) DEFAULT '0' NOT NULL,
+  t3ver_label varchar(255) DEFAULT '' NOT NULL,
+  t3ver_state smallint(6) DEFAULT '0' NOT NULL,
+  t3ver_stage int(11) DEFAULT '0' NOT NULL,
+  t3ver_count int(11) DEFAULT '0' NOT NULL,
+  t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
+  t3_origuid int(11) DEFAULT '0' NOT NULL,
+  PRIMARY KEY (uid)
+);
+
+CREATE TABLE tx_firefighter_domain_model_brigade (
   uid int(11) NOT NULL auto_increment,
   pid int(11) DEFAULT '0' NOT NULL,
   name varchar(255) DEFAULT '' NOT NULL,
@@ -49,10 +86,12 @@ CREATE TABLE tx_rescue_reports_domain_model_car (
   t3ver_count int(11) DEFAULT '0' NOT NULL,
   t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
   t3_origuid int(11) DEFAULT '0' NOT NULL,
+  priority INT DEFAULT 0,
   PRIMARY KEY (uid)
 );
 
-CREATE TABLE tx_rescue_reports_domain_model_brigade (
+-- Station-Tabelle (ursprüngliches ALTER TABLE entfernt, vollständiges CREATE TABLE hinzugefügt)
+CREATE TABLE tx_firefighter_domain_model_station (
   uid int(11) NOT NULL auto_increment,
   pid int(11) DEFAULT '0' NOT NULL,
   name varchar(255) DEFAULT '' NOT NULL,
@@ -75,33 +114,13 @@ CREATE TABLE tx_rescue_reports_domain_model_brigade (
   t3ver_count int(11) DEFAULT '0' NOT NULL,
   t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
   t3_origuid int(11) DEFAULT '0' NOT NULL,
+  brigade INT(11) DEFAULT 0 NOT NULL,
+  sorting INT(11) DEFAULT 0 NOT NULL,
   PRIMARY KEY (uid)
 );
 
-CREATE TABLE tx_rescue_reports_event_car_mm (
-  uid_local int(11) DEFAULT '0' NOT NULL,
-  uid_foreign int(11) DEFAULT '0' NOT NULL,
-  tablenames varchar(255) DEFAULT '' NOT NULL,
-  sorting int(11) DEFAULT '0' NOT NULL
-);
-
-CREATE TABLE tx_rescue_reports_event_type_mm (
-  uid_local int(11) DEFAULT '0' NOT NULL,
-  uid_foreign int(11) DEFAULT '0' NOT NULL,
-  tablenames varchar(255) DEFAULT '' NOT NULL,
-  sorting int(11) DEFAULT '0' NOT NULL
-);
-
-CREATE TABLE tx_rescue_reports_event_station_mm (
-  uid_local int(11) DEFAULT '0' NOT NULL,
-  uid_foreign int(11) DEFAULT '0' NOT NULL,
-  tablenames varchar(255) DEFAULT '' NOT NULL,
-  sorting int(11) DEFAULT '0' NOT NULL
-);
-
-
-
-CREATE TABLE tx_rescue_reports_domain_model_image (
+-- Weitere Tabellen
+CREATE TABLE tx_firefighter_domain_model_image (
   uid int(11) NOT NULL auto_increment,
   pid int(11) DEFAULT '0' NOT NULL,
   name varchar(255) DEFAULT '' NOT NULL,
@@ -128,10 +147,10 @@ CREATE TABLE tx_rescue_reports_domain_model_image (
   PRIMARY KEY (uid)
 );
 
-CREATE TABLE tx_rescue_reports_domain_model_type (
+CREATE TABLE tx_firefighter_domain_model_type (
   uid int(11) NOT NULL auto_increment,
   pid int(11) DEFAULT '0' NOT NULL,
-  name varchar(255) DEFAULT '' NOT NULL,
+  title varchar(255) DEFAULT '' NOT NULL,
   hidden tinyint(4) DEFAULT '0' NOT NULL,
   deleted tinyint(4) DEFAULT '0' NOT NULL,
   tstamp int(11) DEFAULT '0' NOT NULL,
@@ -154,25 +173,31 @@ CREATE TABLE tx_rescue_reports_domain_model_type (
   PRIMARY KEY (uid)
 );
 
-ALTER TABLE tx_rescue_reports_domain_model_station
-  ADD COLUMN name VARCHAR(255) NOT NULL DEFAULT '',
-  ADD COLUMN sys_language_uid int(11) DEFAULT 0 NOT NULL,
-  ADD COLUMN l18n_parent int(11) DEFAULT 0 NOT NULL,
-  ADD COLUMN l18n_diffsource mediumblob NOT NULL,
-  ADD COLUMN t3ver_oid int(11) DEFAULT 0 NOT NULL,
-  ADD COLUMN t3ver_id int(11) DEFAULT 0 NOT NULL,
-  ADD COLUMN t3ver_wsid int(11) DEFAULT 0 NOT NULL,
-  ADD COLUMN t3ver_label varchar(255) DEFAULT '' NOT NULL,
-  ADD COLUMN t3ver_state smallint(6) DEFAULT 0 NOT NULL,
-  ADD COLUMN t3ver_stage int(11) DEFAULT 0 NOT NULL,
-  ADD COLUMN t3ver_count int(11) DEFAULT 0 NOT NULL,
-  ADD COLUMN t3ver_tstamp int(11) DEFAULT 0 NOT NULL,
-  ADD COLUMN starttime int(11) DEFAULT 0 NOT NULL,
-  ADD COLUMN endtime int(11) DEFAULT 0 NOT NULL;
-  ADD COLUMN t3_origuid int(11) DEFAULT 0 NOT NULL;
-  
-  ALTER TABLE tx_rescue_reports_domain_model_event 
-  ADD COLUMN deployments INT(11) DEFAULT 0 NOT NULL;
-
-
+CREATE TABLE tx_firefighter_domain_model_deployment (
+    uid INT(11) NOT NULL AUTO_INCREMENT,
+    pid INT(11) DEFAULT '0' NOT NULL,
+    title VARCHAR(255) DEFAULT '' NOT NULL,
+    description TEXT,
+    date DATETIME DEFAULT NULL,
+    hidden TINYINT(4) DEFAULT '0' NOT NULL,
+    deleted TINYINT(4) DEFAULT '0' NOT NULL,
+    tstamp INT(11) DEFAULT '0' NOT NULL,
+    crdate INT(11) DEFAULT '0' NOT NULL,
+    cruser_id INT(11) DEFAULT '0' NOT NULL,
+    sys_language_uid INT(11) DEFAULT '0' NOT NULL,
+    l18n_parent INT(11) DEFAULT '0' NOT NULL,
+    l18n_diffsource MEDIUMBLOB,
+    starttime INT(11) DEFAULT '0' NOT NULL,
+    endtime INT(11) DEFAULT '0' NOT NULL,
+    t3ver_oid INT(11) DEFAULT '0' NOT NULL,
+    t3ver_id INT(11) DEFAULT '0' NOT NULL,
+    t3ver_wsid INT(11) DEFAULT '0' NOT NULL,
+    t3ver_label VARCHAR(255) DEFAULT '' NOT NULL,
+    t3ver_state SMALLINT(6) DEFAULT '0' NOT NULL,
+    t3ver_stage INT(11) DEFAULT '0' NOT NULL,
+    t3ver_count INT(11) DEFAULT '0' NOT NULL,
+    t3ver_tstamp INT(11) DEFAULT '0' NOT NULL,
+    t3_origuid INT(11) DEFAULT '0' NOT NULL,
+    PRIMARY KEY (uid)
+);
 
